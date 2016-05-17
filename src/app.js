4a8489806b94657d4a71a8914fa8853a6bfd3c64
Roger Dering <rogerdering@hotmail.com>
2016-05-17 15:15:18 +0200

    half done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 var express = require ( 'express')
+var fs = require ('fs' )
 var app = express ( )
+
+app.use(express.static ( './resources/'))
 
 app.set('view engine', 'jade');
 app.set('views', 'src/views');
 
-app.get ('/', ( req, res ) => { 
-	res.render ( 'index' )
+app.get ('/', ( req, res ) => {
+
+	fs.readFile('./resources/books.json', function(error, data) {
+		if (error) {
+			throw error
+		} else {
+			var parsedBooks = JSON.parse ( data )
+			console.log ( parsedBooks )
+			res.render ( 'index', {  books: parsedBooks } )
+		}
+	}) 
+
 })
 
 app.listen ( 3000 )
