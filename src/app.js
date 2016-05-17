129bb4a66b5a93f5c0e700952b68af0082935ca7
Roger Dering <rogerdering@hotmail.com>
2016-05-17 14:33:09 +0200

    using jade

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 var express = require ( 'express')
 var app = express ( )
 
+app.set('view engine', 'jade');
+app.set('views', 'src/views');
+
 app.get ('/', ( req, res ) => { 
-	res.send ( 'I\'m on' )
+	res.render ( 'index )
 })
 
 app.listen ( 3000 )
