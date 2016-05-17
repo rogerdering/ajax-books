4da634bd359435d0c7ed554ce7a1908d797017f5
Roger Dering <rogerdering@hotmail.com>
2016-05-17 16:00:09 +0200

    added the DOM

---
+++ resources/js/custom.js
@@ -0,0 +1,7 @@
+$ ( document ) .ready ( function ( ) {
+	console.log ( "DOM is ready" )
+	$ ( '.thumb' ).on ( 'click', function ( ) {
+
+		console.log ( "you sir(ina), clocked an image!")
+	} )
+} )
